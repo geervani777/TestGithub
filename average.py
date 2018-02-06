0ca65171c6a1394db740231deff11aeb19ba9ea5
geervani777 <35727718+geervani777@users.noreply.github.com>
2018-02-06 17:14:49 +0530

    Create average.py

---
+++ average.py
@@ -0,0 +1,26 @@
+print("enter the  all subjects marks:")
+telugu=int(input("enter telugu marks:"))
+english=int(input("enter english marks:"))
+social=int(input("enter social marks:"))
+sum=telugu+english+social
+print(sum)
+avg=sum/3
+print(avg)
+percentage=(sum/300)*100
+a=percentage
+print(a)
+print("enter the all subjects marks:")
+telugu=int(input("enter telugu marks:"))
+english=int(input("enter english marks:"))
+social=int(input("enter social marks:"))
+sum=telugu+english+social
+print(sum)
+avg=sum/3
+print(avg)
+percentage=(sum/300)*100
+b=percentage
+print(b)
+if a >= b:
+     print("a is the top")
+else:
+    print(" b is the top")
