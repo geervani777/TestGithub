96256e4e12463f5d5a7500c5f391fade52fc2388
geervani777 <35727718+geervani777@users.noreply.github.com>
2018-02-06 16:33:13 +0530

    Create btc5.py

---
+++ btc5.py
@@ -0,0 +1,62 @@
+import requests
+import json
+btc = []
+c = []
+inp = 0
+i=0
+while 1:
+    i+=1
+    start = input("enter the start date :")
+    url = "https://api.coindesk.com/v1/bpi/historical/close.json?start="+ start + "&end=" + start                                                                                    
+    response = requests.get(url)
+    data = response.text
+    #print(data)
+    bpi = data.split(":")
+    bpi2 = bpi[2]
+    bpi3 = bpi2.split("}")
+    bpi4 =bpi3[0]
+    print(bpi4)
+    inp = input('Enter the btc%d: '%i)
+    c.append(inp)
+    #print(c)
+    mul = float(bpi4) * float(inp)
+    print(mul)
+    btc.append(mul)
+    #print (btc)         
+    #print (c)
+    sum = 0
+    sum1 =0
+    for b in btc:
+        sum = sum + float(b)
+        print("sum")
+        print(sum)
+    for b1 in c:
+        sum1 = sum1 + float(b1)
+        print("sum1")
+        print(sum1)
+    data = requests.get("https://community-coinbase.p.mashape.com/prices/spot_rate?api_key=0972c2cef9274307b25e1b025fadb09b",
+    headers={
+            "X-Mashape-Key": "Ejm5wx8Wu0mshuTbUpPSGOF0q5QJp1xAMM2jsnwjiWFQv4s6SQ",
+            "Accept": "application/json"
+    }
+    )
+    USDPrice = data.content
+    #print(USDPrice)
+    UP = USDPrice.decode("utf-8")
+    UP1 = UP.split(":")
+    UP2 = UP1[1].split(",")
+    USD = UP2[0]
+    USD1 = USD.strip('"')
+    print( "Todays btc price:",USD1)            
+    usd = (float(USD1) * sum1)-sum
+    print("Todays price into sum of the btc value minus  total of the btc:",usd)
+    print("Todays profit in usd:",usd)
+    url = "https://v3.exchangerate-api.com/bulk/ec270be7fdb11c97eb23d948/USD"
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
+    inr_rate = parsed["rates"]["INR"]
+    INRPrice = float(usd) * parsed["rates"]["INR"]
+    print("Todays profit in INR:",INRPrice)            
+          
+                
